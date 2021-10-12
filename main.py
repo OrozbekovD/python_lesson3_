212de1fcc184d4dc15788648c98a6ab7651532fd
Dosoy <olaf.meister@mail.ru>
2021-10-12 20:46:31 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,32 @@
+dateOfBirthday = 1994  # Camel case
+date_of_birthday = 1993  # Snake case
+var_int = 1  # Variable integer (число)
+var_float = 1.2  # Variable float(числа с плавающей точкой)
+var_str = "Murat"  # Variable string (строка)
+var_boolean_true = True  # Variable boolean(Истина)
+var_boolean_false = False  # Variable boolean(Ложь)
+var_list = [1, 2, 3, "Murat"]  # Variable list (список)
+var_tuple = (1, [1, 2], "Murat",)  # Variable tuple(кортеж)
+var_dict = {"name": "Murat", "date_of_birthday": "02.02.1999", "course": 3,
+            "physical_data": [90, 190], "python": True}  # Variable dictionary(Словарь)
+
+
+def print_hi():
+    dateOfBirthday = 1999  # Camel case
+    date_of_birthday = 1998  # Snake case
+    var_int = 2  # Variable integer (число)
+    var_float = 1.2  # Variable float(числа с плавающей точкой)
+    var_str = "Dasya"  # Variable string (строка)
+    var_boolean_true = True  # Variable boolean(Истина)
+    var_boolean_false = False  # Variable boolean(Ложь)
+    var_list = [1, 2, 3, "Dasya"]  # Variable list (список)
+    var_tuple = (1, [1, 2], "Dasya",)  # Variable tuple(кортеж)
+    var_dict = {"name": "Dasya", "date_of_birthday": "02.02.1991", "course": 4,
+                "physical_data": [80, 180], "python": True}  # Variable dictionary(Словарь)
+
+    print(date_of_birthday)
+    print(var_int)
+
+
+if __name__ == '__main__':
+    print()
